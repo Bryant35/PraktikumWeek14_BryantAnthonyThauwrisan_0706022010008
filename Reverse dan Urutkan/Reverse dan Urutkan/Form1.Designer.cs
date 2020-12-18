c2fe2c89f72652c3fad8dd367e1449e7d94d0b5e
Bryant A. T <banthony@student.ciputra.ac.id>
2020-12-18 11:20:01 +0800

    PraktikumWeek14_BryantAnthonyThauwrisan_0706022010008

---
+++ Reverse dan Urutkan/Reverse dan Urutkan/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿
+namespace Reverse_dan_Urutkan
+{
+    partial class FormBoxString
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelInput = new System.Windows.Forms.Label();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.buttonReverse = new System.Windows.Forms.Button();
+            this.buttonUrutkan = new System.Windows.Forms.Button();
+            this.labelOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelInput
+            // 
+            this.labelInput.AutoSize = true;
+            this.labelInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.labelInput.Location = new System.Drawing.Point(31, 36);
+            this.labelInput.Name = "labelInput";
+            this.labelInput.Size = new System.Drawing.Size(46, 20);
+            this.labelInput.TabIndex = 0;
+            this.labelInput.Text = "Input";
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBoxInput.Location = new System.Drawing.Point(35, 59);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(324, 26);
+            this.textBoxInput.TabIndex = 1;
+            this.textBoxInput.TextChanged += new System.EventHandler(this.textBoxInput_TextChanged_1);
+            this.textBoxInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxInput_KeyDown);
+            // 
+            // buttonReverse
+            // 
+            this.buttonReverse.Location = new System.Drawing.Point(35, 186);
+            this.buttonReverse.Name = "buttonReverse";
+            this.buttonReverse.Size = new System.Drawing.Size(140, 29);
+            this.buttonReverse.TabIndex = 2;
+            this.buttonReverse.Text = "REVERSE";
+            this.buttonReverse.UseVisualStyleBackColor = true;
+            this.buttonReverse.Click += new System.EventHandler(this.buttonReverse_Click);
+            this.buttonReverse.KeyDown += new System.Windows.Forms.KeyEventHandler(this.buttonReverse_KeyDown);
+            // 
+            // buttonUrutkan
+            // 
+            this.buttonUrutkan.Location = new System.Drawing.Point(216, 186);
+            this.buttonUrutkan.Name = "buttonUrutkan";
+            this.buttonUrutkan.Size = new System.Drawing.Size(143, 29);
+            this.buttonUrutkan.TabIndex = 3;
+            this.buttonUrutkan.Text = "URUTKAN";
+            this.buttonUrutkan.UseVisualStyleBackColor = true;
+            this.buttonUrutkan.Click += new System.EventHandler(this.buttonUrutkan_Click);
+            this.buttonUrutkan.KeyDown += new System.Windows.Forms.KeyEventHandler(this.buttonUrutkan_KeyDown);
+            // 
+            // labelOutput
+            // 
+            this.labelOutput.AutoSize = true;
+            this.labelOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.labelOutput.Location = new System.Drawing.Point(35, 338);
+            this.labelOutput.Name = "labelOutput";
+            this.labelOutput.Size = new System.Drawing.Size(71, 25);
+            this.labelOutput.TabIndex = 4;
+            this.labelOutput.Text = "Output";
+            this.labelOutput.Click += new System.EventHandler(this.labelOutput_Click);
+            // 
+            // FormBoxString
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelOutput);
+            this.Controls.Add(this.buttonUrutkan);
+            this.Controls.Add(this.buttonReverse);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.labelInput);
+            this.Name = "FormBoxString";
+            this.Text = "String";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelInput;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.Button buttonReverse;
+        private System.Windows.Forms.Button buttonUrutkan;
+        private System.Windows.Forms.Label labelOutput;
+    }
+}
+
---
+++ Reverse dan Urutkan/Reverse dan Urutkan/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Reverse_dan_Urutkan
+{
+    public partial class FormBoxString : Form
+    {
+        public FormBoxString()
+        {
+            InitializeComponent();
+            this.ActiveControl = textBoxInput;
+            textBoxInput.Focus();
+        }
+        private void textBoxInput_TextChanged_1(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBoxInput_KeyDown(object sender, KeyEventArgs e)
+        {
+            string input = textBoxInput.Text;
+            if(e.KeyCode == Keys.Enter)
+            {
+                if (input == String.Empty)
+                {
+                    MessageBox.Show("Tolong isi textbox terlebih dahulu!");
+                    labelOutput.Text = "Output";
+                }
+                else 
+                {
+                    this.ActiveControl = buttonReverse;
+                    buttonReverse.Focus();
+                }
+            }
+        }
+
+        private void buttonReverse_Click(object sender, EventArgs e)
+        {
+            string s1 = textBoxInput.Text;
+            char[] urutan = s1.ToCharArray();
+            string reverse = "";
+            for (int i = urutan.Length - 1; i > -1; i--)
+            {
+                reverse = reverse + urutan[i];
+            }
+            labelOutput.Text = reverse;
+            textBoxInput.Clear();
+            this.ActiveControl = textBoxInput;
+            textBoxInput.Focus();
+        }
+
+        private void buttonReverse_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                labelOutput_Click(sender, e);
+            }
+            else if (e.KeyCode == Keys.Left)
+            {
+                this.ActiveControl = buttonUrutkan;
+                buttonUrutkan.Focus();
+            }
+        }
+
+        private void buttonUrutkan_Click(object sender, EventArgs e)
+        {
+            string s1 = textBoxInput.Text;
+            char[] urutan = s1.ToCharArray();
+            string urut = "";
+            string urutKapital = "";
+            string urutSimbol = "";
+            char urutsimpan;
+            for (int i = urutan.Length - 1; i > -1; i--)
+            {
+                for(int j = urutan.Length - 1; j > -1; j--)
+                {
+                    if (urutan[i] != ' ')
+                    {
+                        if (urutan[i] > urutan[j])
+                        {
+                            urutsimpan = urutan[i];
+                            urutan[i] = urutan[j];
+                            urutan[j] = urutsimpan;
+                        }
+                    }
+                }
+                if (urutan[i] >= 'a')
+                {
+                    urut = urut + urutan[i];
+                    urutan[i] = 'z';                   
+                }
+                else if (urutan[i] <= 'Z')
+                {
+                    urutKapital = urutKapital + urutan[i];
+                    urutan[i] = 'z';
+                }
+                else if (urutan[i] < 'A')
+                {
+                    urutSimbol = urutSimbol + urutan[i];
+                    urutan[i] = 'z';
+                }
+            }
+            labelOutput.Text = urut + urutKapital + urutSimbol;
+            textBoxInput.Clear();
+            this.ActiveControl = textBoxInput;
+            textBoxInput.Focus();
+        }
+
+        private void buttonUrutkan_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                labelOutput_Click(sender, e);
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                this.ActiveControl = buttonReverse;
+                buttonReverse.Focus();
+            }
+        }
+
+        private void labelOutput_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
